03786642ab2eeeb9bf808eeca30895787d36c495
xzsfd2457 <37336692+xzsfd2457@users.noreply.github.com>
2018-03-21 06:28:49 +0000

    Update print holiday.py

--- Player/codekata/set-1/print holiday.py
+++ Player/codekata/set-1/print holiday.py
@@ -1,12 +1,7 @@
 a=raw_input()
-b=raw_input()
 c='sunday'
 d='saturday'
 if (a==c or a==d):
-        print('yes')
-else:
-        print('No')
-if (b==c or b==d):
         print('yes')
 else:
         print('No')
