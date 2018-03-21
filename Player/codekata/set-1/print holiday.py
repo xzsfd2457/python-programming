fc829d26b709951cbacd660dc87d854bf748a2d2
xzsfd2457 <37336692+xzsfd2457@users.noreply.github.com>
2018-03-21 06:24:12 +0000

    Create print holiday.py

---
+++ Player/codekata/set-1/print holiday.py
@@ -0,0 +1,12 @@
+a=raw_input()
+b=raw_input()
+c='sunday'
+d='saturday'
+if (a==c or a==d):
+        print('Holiday')
+else:
+        print('Not a Holiday')
+if (b==c or b==d):
+        print('Holiday')
+else:
+        print('Not a Holiday')
